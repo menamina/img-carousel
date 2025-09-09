0a67962713e429f2cc1ab4827b6b383d8bda79c4
Mena <dayjacs03@gmail.com>
2025-09-08 19:52:32 -0500

    overflow is now hidden

--- src/index.js
+++ src/index.js
@@ -4,13 +4,33 @@ import art2 from './art2.jpg';
 import art3 from './art3.jpg';
 import art4 from './art4.jpg';
 import art5 from './art5.jpg';
+import { dotClick } from './dotClick';
 
 const images = [art1, art2, art3, art4, art5];
-
-const imgQueue = document.querySelector('.imgQueue');
+const container = document.querySelector('.imgQueue');
 
 images.forEach(src => {
   const img = document.createElement('img');
   img.src = src;
-  imgQueue.appendChild(img);
+  img.classList.add("picture");
+  container.appendChild(img);
+});
+
+const dots = document.querySelector(".dots");
+dots.addEventListener('click', (e) => {
+    const selectedDot = e.target.dataset.index;
+    dotClick(selectedDot);
+
+})
+
+
+const rightArrow = document.querySelector(".right");
+const leftArrow = document.querySelector(".left");
+
+rightArrow.addEventListener('click', () => {
+
+});
+
+leftArrow.addEventListener('click', () => {
+
 });
